dadd9e0a1581aa1970bed630ed3284dfb1601e83
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-15 06:07:34 +0700

    shadcn ui setup

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div>
+      <Button>This is a Button</Button>
+    </div>
+  );
+};
+
+export default Home;
